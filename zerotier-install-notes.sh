928c0c3cedffabdd14ef3e31dee024a99e653486
Bert <bvdl@me.com>
2021-02-19 21:48:04 +0100

    Update zerotier-install-notes.sh

--- zerotier-install-notes.sh
+++ zerotier-install-notes.sh
@@ -8,5 +8,4 @@ cd ZeroTierOne
 make && make selftest && make install
 
 wget https://raw.githubusercontent.com/bvdlingen/solus-linux-scripts/master/zerotier.service -O /etc/systemd/system/zerotier.service
-
 systemctl enable zerotier && systemctl start zerotier && systemctl status zerotier
