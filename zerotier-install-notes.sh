2a3d230072b9b43b00c2bbc9f1db8fec6f44cdd8
Bert <bvdl@me.com>
2021-02-19 22:03:09 +0100

    Update zerotier-install-notes.sh

--- zerotier-install-notes.sh
+++ zerotier-install-notes.sh
@@ -1,5 +1,5 @@
 sudo su
-eopkg install systemd-devel llvm-clang -y
+eopkg install git systemd-devel llvm-clang -y
 
 mkdir /opt && cd /opt 
 
@@ -9,3 +9,6 @@ make && make selftest && make install
 
 wget https://raw.githubusercontent.com/bvdlingen/solus-linux-scripts/master/zerotier.service -O /etc/systemd/system/zerotier.service
 systemctl enable zerotier && systemctl start zerotier && systemctl status zerotier
+
+cd /opt
+rm -rf ZeroTierOne
